811a187c7db4edd3497fea8c1d397eaa679cd1aa
Aishwarya-rathudi <x21222762@student.ncirl.ie>
2024-04-01 18:26:09 +0530

    added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Insurance_premium-prediction"
+AUTHOR_USER_NAME = "Aishwarya-rathudi"
+SRC_REPO = "Insurance_prediction"
+AUTHOR_EMAIL = "x21222762@student.ncirl.ie"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="my Insurance_prediction project using MLOPS",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
